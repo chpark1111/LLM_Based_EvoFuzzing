f030faa2b438b417e8027f93b2d7986a8aa35506
Chanhyeok <chpark1111@kaist.ac.kr>
2023-12-03 05:01:48 +0900

    done intelchat

--- src/evollmfuzz/llm/intelchat.py
+++ src/evollmfuzz/llm/intelchat.py
@@ -1,35 +1,35 @@
 import transformers
+import torch
+import torch.nn as nn
+
 from src.check import check_valid_input
 
+device = "cpu"
 model_name = 'Intel/neural-chat-7b-v3-1'
 model = transformers.AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+model = model.to(device)
 
 def generate_response(system_input, user_input):
 
-    # Format the input using the provided template
     prompt = f"### System:\n{system_input}\n### User:\n{user_input}\n### Assistant:\n"
+    inputs = tokenizer.encode(prompt, return_tensors="pt", add_special_tokens=False).to(device)
 
-    # Tokenize and encode the prompt
-    inputs = tokenizer.encode(prompt, return_tensors="pt", add_special_tokens=False)
-
-    # Generate a response
     outputs = model.generate(inputs, max_length=1000, num_return_sequences=1)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    # Extract only the assistant's response
     return response.split("### Assistant:\n")[-1]
 
 
 def mutatate_input_with_llm(equation, num_gen):
     system_input = "You are a math expert assistant. Your mission is to help users generate math equations. Do not include any other words. You must change numbers and function, and also the structure of the equation."
-    user_input = "Generate %d equations like: %s"%(num_gen, equation)
+    user_input = "Generate %d equations (not same) like: %s"%(num_gen, equation)
 
     input_strings = set()
     while len(input_strings) != num_gen:
         response = generate_response(system_input, user_input)
         print(response)
-        possible_strings = list(map(lambda x: x.strip(), response.split('\n')[1:]))
+        possible_strings = list(map(lambda x: x[3:].strip(), response.split('\n')))
         print(possible_strings)
         for str_input in possible_strings:
             result = check_valid_input(str_input)
@@ -42,6 +42,6 @@ def mutatate_input_with_llm(equation, num_gen):
     return list(input_strings)
 
 if __name__ == "__main__":
-    user_input = "((sqrt(-10) + -13) * sqrt(-13))"
-    num_gen = 10
+    user_input = "(1 - ((-15 - cos(sin(-13))) - cos(cos(-237))))"
+    num_gen = 5
     print(mutatate_input_with_llm(user_input, num_gen))
